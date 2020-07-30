63dba247135cb17d78a64ba91c84e2a54e0b4c32
Proyol <52104910+Proyol@users.noreply.github.com>
2020-07-30 14:24:59 -0300

    Create Combinações possíveis de bits.c
    Algoritmo de combinações possíveis de bits em uma sequência de n elementos. Exercício 8 da lista de complexidade de algoritmos

---
+++ Complexidade de Algoritmos/Combinações possíveis de bits.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Algoritmo de complexidade O(2^n)
+void printBitsSequence(int n){
+    int seq[n];
+    int i, j, k;
+    int power = pow(2, n);
+
+    //Uma sequência de n termos terá 2^n combinações possíveis
+    for(i = 0; i < power; i++){
+        k = i;
+        //Começar a inserção dos bits de traz para frente na sequência, como aprendemos a calcular
+        for(j = n-1; j >= 0; j--){
+            seq[j] = k % 2;
+            k = k / 2;
+        }
+        for(j = 0; j < n; j++){
+            printf("%d", seq[j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int n;
+
+    printf("Informe o tamanho da sequencia: ");
+    scanf("%d", &n);
+
+    printBitsSequence(n);
+}
