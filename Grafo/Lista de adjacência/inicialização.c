35ee59630db72a87148e4df1730aaeac1da0df38
Daniel Favero <52104910+Proyol@users.noreply.github.com>
2020-09-25 14:30:05 -0300

    Update inicialização.c
    Atualizado funções

--- Grafo/Lista de adjacência/inicialização.c
+++ Grafo/Lista de adjacência/inicialização.c
@@ -1,34 +1,27 @@
-//a função que inicia a matriz de adjacência recebe como parâmetro o número de vértices (nós)
-//a matriz de adjacência é uma matriz n x n
-static int **startAdjacentMatrix(int n)
+//A função recebe como parametro o número de vértices e retorna um vetor com n posições
+//Cada posição do vetor é uma lista encadeada que representa as arestas
+static List **startAdjacentList(int n)
 {
-    int i, j;
-    int **matrix = (int **)malloc(n * sizeof(int *));
+    int i;
+    List **l = (List **)malloc(n * sizeof(List *));
 
     for (i = 0; i < n; i++)
     {
-        matrix[i] = (int *)malloc(n * sizeof(int));
+        l[i] = (List *)malloc(sizeof(List));
+        l[i]->head = NULL;
     }
 
-    //iniciar todas as posições com 0
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            matrix[i][j] = 0;
-        }
-    }
-
-    return matrix;
+    return l;
 }
 
-//a função recebe o número de vértices que o grafo terá
-Graph *startGraph(int vertices)
+//Iniciar o grafo
+static Graph *startGraph(int vertices)
 {
     Graph *g = (Graph *)malloc(sizeof(Graph));
     g->vertices = vertices;
     g->edges = 0;
-    g->adjacentMatrix = startAdjacentMatrix(vertices);
+    g->adjacentList = startAdjacentList(vertices);
 
     return g;
 }
+
