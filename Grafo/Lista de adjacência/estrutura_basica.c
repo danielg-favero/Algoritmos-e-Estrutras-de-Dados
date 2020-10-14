19d2fd0ef5c3f2c9cbe23c205f68fe593b5154a3
Daniel Favero <52104910+Proyol@users.noreply.github.com>
2020-10-14 10:25:18 -0300

    feat: atulizado estrutura de grafo AL
    Adicionado vetor de visitado e pais para vértices de um grafo

--- Grafo/Lista de adjacência/estrutura_basica.c
+++ Grafo/Lista de adjacência/estrutura_basica.c
@@ -1,10 +1,12 @@
 //Estrutura básica para uma grafo representado por lista de adjacência
 //Essa estrutura requer uma lista encadeada simples para representar as arestas entre dois vértices
 
-struct Graph
+struct GraphAL
 {
     int edges;    //número de arestas
     int vertices; //número de vertices;
+    int *fathers;
+    int *visited;
 
     //lista de adjacência
     //ela mostra as arestas que conectam um vértice a outro
